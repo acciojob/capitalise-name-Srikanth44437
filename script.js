58dc1fda267da2cb7fb5554d50065a14e283eb85
Srikanth44437 <113106500+Srikanth44437@users.noreply.github.com>
2024-01-24 12:59:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function capitalizeName()
+{
+	var inputField = document.getElementById("fname");
+	inputField.addEventListener("blur", function() {
+		
+		inputField.value = inputField.value.toUpperCase();
+	});
+}
+capitalizeName();
